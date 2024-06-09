3fd059b409e823e348a84f499e5b4101ebd5d1a1
Noah <Noah@DAS-LAPTOP>
2024-06-09 03:25:42 +0200

    Gui Commit 2

--- Spotify_Game/View/ViewModel/MainViewModel.cs
+++ Spotify_Game/View/ViewModel/MainViewModel.cs
@@ -1,15 +1,47 @@
-﻿using System;
+﻿using Spotify_Game.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Spotify_Game.View.ViewModel
 {
-     class MainViewModel
-    {
-        public MainViewModel() {
-            
+
+  
+     class MainViewModel : ObservableObject
+     {
+
+        public RelayCommand  HomeViewCommand { get; set; } 
+        public RelayCommand ProfileViewCommand { get; set; }
+        
+        public HomeViewModel HomeVM { get; set; }
+        public ProfileViewModel ProfileVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
         }
-    }
+               
+        public MainViewModel() {
+
+            HomeVM = new HomeViewModel();
+            ProfileVM = new ProfileViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => {
+                CurrentView = HomeVM;
+            });
+            ProfileViewCommand = new RelayCommand(o => {
+                CurrentView = ProfileVM;
+            });
+        }
+     }
 }
