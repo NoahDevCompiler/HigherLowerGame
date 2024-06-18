4fceabfc82ac12a3c2c1b4afa81dbd4ac12a53cc
Noah <Noah@DAS-LAPTOP>
2024-06-18 18:01:01 +0200

    iuaerg

---
+++ Spotify_Game/Core/BindableImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Spotify_Game.Core
+{
+    public class BindableImage : Image
+    {
+        static BindableImage() {
+            SourceProperty.OverrideMetadata(typeof(BindableImage), new FrameworkPropertyMetadata(OnSourceChanged));
+        }
+
+        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            var image = d as BindableImage;
+            image?.OnSourceChanged(e);
+        }
+
+        protected virtual void OnSourceChanged(DependencyPropertyChangedEventArgs e) {
+        
+            var rightImageStoryboard = (Storyboard)FindResource("RightImageSlideInStoryboard");
+            rightImageStoryboard?.Begin();
+        }
+    }
+}
--- Spotify_Game/View/GameView.xaml.cs
+++ Spotify_Game/View/GameView.xaml.cs
@@ -2,6 +2,7 @@
 using Spotify_Game.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Spotify_Game.View
 {
@@ -24,46 +26,116 @@ namespace Spotify_Game.View
     /// </summary>
     public partial class GameView : Window
     {
-        private List<VideoModel> _videos= new List<VideoModel>();
+        private List<VideoModel> _videos = new List<VideoModel>();
         private int _currentIndex = 0;
-        public GameView()
-        {
+        int _score = 0;
+
+        public GameView() {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
-           
         }
-        private async void MainWindow_Loaded(object sender, RoutedEventArgs e) {
-            await DisplayVideo();
-        }
-        private async Task DisplayVideo() {
 
-            _videos = await YoutubeService.GetAsyncVideo(500);
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            _videos = await YoutubeService.GetAsyncVideo(50);
             _videos.Shuffle();
+            DisplayVideo();
+        }
+
+        public void DisplayVideo() {
+
+            versus.Visibility = Visibility.Visible;
+            tick.Visibility = Visibility.Collapsed;
+            cross.Visibility = Visibility.Collapsed;
+
             var left = _videos[_currentIndex];
             var right = _videos[_currentIndex + 1];
 
             LeftTitle.Text = left.snippet.Title;
             LeftChannel.Text = string.Join(", ", left.snippet.ChannelTitle);
-            LeftViews.Text = $"Views: {left.statistics.ViewCount}";
+            LeftViews.Text = $"Views: {int.Parse(left.statistics.ViewCount):N0}";
             LeftImage.Source = new BitmapImage(new Uri(left.snippet.ThumbnailUrl));
 
             RightTitle.Text = right.snippet.Title;
             RightChannel.Text = string.Join(", ", right.snippet.ChannelTitle);
-            RightViews.Text = $"Views: {right.statistics.ViewCount}";
-            RightImage.Source = new BitmapImage(new Uri(right.snippet.ThumbnailUrl));
-
-        }
-        private void CloseButton_Click(object sender, RoutedEventArgs e) {
            
-            Close(); 
+            RightImage.Source = new BitmapImage(new Uri(right.snippet.ThumbnailUrl));
         }
 
-        private void MoreButton_Click(object sender, RoutedEventArgs e) {
-            var leftTrack = _videos[_currentIndex];
-            var rightTrack = _videos[_currentIndex + 1];  
-            
+        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private async void HigherButton_Click(object sender, RoutedEventArgs e) {
+
+            var left = Convert.ToInt32(_videos[_currentIndex].statistics.ViewCount);
+            var right = Convert.ToInt32(_videos[_currentIndex + 1].statistics.ViewCount);
+
+            string formattedViewCount = right.ToString("N0");
+            await AnimateViewCount(right);
+
+            if (left <= right) {
+                _score++;
+                score.Text = Convert.ToString(_score);
+
+                //UI Element
+                tick.Visibility = Visibility.Visible;
+                Thread.Sleep(1000);
+
                 _currentIndex++;
-            DisplayVideo();
+                RightViews.Text = "";
+                DisplayVideo();
+            }
+            else {
+
+                MessageBox.Show("Leider Sie Haben Falsch Batman Jack mit Passwort 1234");
+                this.Close();
+              
+            }
+            
+          
+        }
+        private async void LowerButton_Click(object sender, RoutedEventArgs e) {
+
+            var left = Convert.ToInt32(_videos[_currentIndex].statistics.ViewCount);
+            var right = Convert.ToInt32(_videos[_currentIndex + 1].statistics.ViewCount);
+
+            string formattedViewCount = right.ToString("N0");
+            await AnimateViewCount(right);
+
+            if (left >= right) {
+                _score++;
+                score.Text = Convert.ToString(_score);
+
+                //UI Ecplise Update
+                cross.Visibility = Visibility.Visible;
+
+                Thread.Sleep(1000);
+               
+                _currentIndex++;
+                RightViews.Text = "";
+                DisplayVideo();
+
+            } else {
+                MessageBox.Show("Leider Sie Haben Falsch Batman Jack mit Passwort 1234");
+                this.Close();
+            }
+
+        }
+
+        private async Task AnimateViewCount(int targetValue) {
+            int startValue = 0;
+            int totalFrames = 60;
+
+            for (int i = 0; i <= totalFrames; i++) {
+                double progress = (double)i / totalFrames;
+                int currentCount = (int)(startValue + (targetValue - startValue) * progress);
+                RightViews.Text = currentCount.ToString("N0");
+                await Task.Delay(1000 / 60);
+            }
+
+            RightViews.Text = targetValue.ToString("N0");
+            
         }
     }
 }
+    
